cab0a988d1d7cea3e5f32171d5892e73d679cffc
pjonnathan <jonnathanmatos@gmail.com>
2023-11-06 11:24:10 -0300

    Commit 3 - Mais Extract Functions

--- index.js
+++ index.js
@@ -1,54 +1,62 @@
 const { readFileSync } = require('fs');
 
 function gerarFaturaStr (fatura, pecas) {
+    
+    function formatarMoeda(valor) {
+      return new Intl.NumberFormat("pt-BR",
+        { style: "currency", currency: "BRL",
+          minimumFractionDigits: 2 }).format(valor/100);
+    }
     let totalFatura = 0;
     let creditos = 0;
     let faturaStr = `Fatura ${fatura.cliente}\n`;
-    const formato = new Intl.NumberFormat("pt-BR",
-                          { style: "currency", currency: "BRL",
-                            minimumFractionDigits: 2 }).format;
+    const formato = formatarMoeda
+
     function getPeca(apresentacao) {
       return pecas[apresentacao.id];
     }
 
-    function calcularTotalApresentacao(apre) {
-      for (apre of fatura.apresentacoes) {
-        //const peca = getPeca(apre);
+    function calcularCredito(apre) {
+      let creditos = 0;
+      creditos += Math.max(apre.audiencia - 30, 0);
+      if (getPeca(apre).tipo === "comedia") 
+         creditos += Math.floor(apre.audiencia / 5);
+      return creditos;   
+    }
 
-        getPeca(apre);
+    for (apre of fatura.apresentacoes) {
+      getPeca(apre);
+      let total = 0;
 
-        let total = 0;
-        switch (getPeca.tipo) {
-        case "tragedia":
-          total = 40000;
-          if (apre.audiencia > 30) {
-            total += 1000 * (apre.audiencia - 30);
-          }
-          break;
-        case "comedia":
-          total = 30000;
-          if (apre.audiencia > 20) {
-            total += 10000 + 500 * (apre.audiencia - 20);
-          }
-          total += 300 * apre.audiencia;
-          break;
-        default:
-            throw new Error(`Peça desconhecia: ${getPeca.tipo}`);
-        }
+      function calcularTotalApresentacao(apre) {
         
-    
-        // créditos para próximas contratações
-        creditos += Math.max(apre.audiencia - 30, 0);
-        if (getPeca.tipo === "comedia") 
-          creditos += Math.floor(apre.audiencia / 5);
-    
-        // mais uma linha da fatura
-        faturaStr += `  ${getPeca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
-        totalFatura += total;
+          switch (getPeca(apre).tipo) {
+          case "tragedia":
+            total = 40000;
+            if (apre.audiencia > 30) {
+              total += 1000 * (apre.audiencia - 30);
+            }
+            break;
+          case "comedia":
+            total = 30000;
+            if (apre.audiencia > 20) {
+              total += 10000 + 500 * (apre.audiencia - 20);
+            }
+            total += 300 * apre.audiencia;
+            break;
+          default:
+              throw new Error(`Peça desconhecia: ${getPeca(apre).tipo}`);
+          }
+          return total;
 
-        return total;
-
-      }
+        }
+          // créditos para próximas contratações
+          calcularCredito(apre)
+      
+          // mais uma linha da fatura
+          faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+          totalFatura += total;
+ 
     }
     
     faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
@@ -57,10 +65,7 @@ function gerarFaturaStr (fatura, pecas) {
     
   }
 
-let total = calcularTotalApresentacao(apre, peca);
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas = JSON.parse(readFileSync('./pecas.json'));
 const faturaStr = gerarFaturaStr(faturas, pecas);
 console.log(faturaStr);
-
-console.log(total)
